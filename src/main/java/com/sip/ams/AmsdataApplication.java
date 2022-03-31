e665cfdb9d9a3b38ad8b32b3dc0abec2b69cf8f2
saidi marouene <saidi.marouen@gmail.com>
2022-03-31 12:29:49 +0100

    last commit

--- src/main/java/com/sip/ams/AmsdataApplication.java
+++ src/main/java/com/sip/ams/AmsdataApplication.java
@@ -1,13 +1,21 @@
 package com.sip.ams;
 
+import java.io.File;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.sip.ams.controllers.ArticleController;
 
 @SpringBootApplication
 public class AmsdataApplication {
 
 	public static void main(String[] args) {
+		new File(ArticleController.uploadDirectory).mkdir();
 		SpringApplication.run(AmsdataApplication.class, args);
 	}
 
 }
+
+
+
